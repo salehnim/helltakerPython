44016bafb0aefca10d6baa555a8a0f85dccbb923
salehnim <104429831+salehnim@users.noreply.github.com>
2022-06-15 22:11:59 +0200

    Delete Python/SokobanSimple directory

--- Python/SokobanSimple/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-actions = {d : Action('move',d) for d in 'udrl'} | {d.upper() : Action('push',d) for d in 'udrl'}
-s0 = {'me': (3, 3),
-'boxes': {(3, 4), (3, 2)}}
-map_rules = {'goals': {(3, 1), (3,5)}, 
-'walls': {},'actions': actions}
