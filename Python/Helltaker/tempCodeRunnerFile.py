13b263543a2713728c55626189083949d5a62ee5
salehnim <104429831+salehnim@users.noreply.github.com>
2022-06-15 22:11:48 +0200

    Delete Python/Helltaker directory

--- Python/Helltaker/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-            if (X1 in blocks) and free(X2, map_rules) and not (X2 in blocks) :
-                temp = {'blocks' : {X2} | blocks - {X1} ,'hero' : X1, 'mobs' : mobs}
-                return temp
-            else :
-                return None
